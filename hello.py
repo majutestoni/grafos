9063915115a33b69eb332c598489bdc1bdabc8f3
majutestoni <majuliatestoni@gmail.com>
2024-06-29 17:00:55 -0300

    ajustes

--- hello.py
+++ hello.py
@@ -4,17 +4,18 @@ import matplotlib.pyplot as plt
 
 df = pd.read_excel('coadm-05-2024.xlsx', sheet_name='planilha')
 
-sigla = df['Sigla']
+siglas = df['Sigla']
 
 grafo = nx.Graph()
-grafo.add_node("A")
-grafo.add_node("B")
-grafo.add_node("C")
+teste = ["A", "B", "C", "D"]
 
-grafo.add_edge("A", "B")
-grafo.add_edge("A", "B")
-grafo.add_edge("C", "C")
-grafo.add_edge("A", "C")
+for node in sigla:
+    grafo.add_node(node)
+
+# grafo.add_edge("A", "B")
+# grafo.add_edge("A", "B")
+# grafo.add_edge("C", "C")
+# grafo.add_edge("A", "C")
 
 nx.draw(grafo, with_labels=True)
 plt.show()
