1cb0ad7cecc15548e5034efb5ff73608fdd84fba
majutestoni <majuliatestoni@gmail.com>
2024-06-29 18:16:38 -0300

    ajustes

--- hello.py
+++ hello.py
@@ -1,21 +1,58 @@
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
+import sys
 
-df = pd.read_excel('coadm-05-2024.xlsx', sheet_name='planilha')
+df = pd.read_excel('teste.xlsx', sheet_name='planilha')
 
-siglas = df['Sigla']
+siglas = df['Sigla'].tolist()
+conselheiros = df['Conselheiro(a)'].tolist()
+orgao = df['Órgão Indicante'].tolist()
 
 grafo = nx.Graph()
-teste = ["A", "B", "C", "D"]
 
-for node in sigla:
-    grafo.add_node(node)
+# Criando um dicionário para mapear cada valor único para uma cor
+cores = {}
+cores.update({node: 'blue' for node in siglas})
+cores.update({node: 'red' for node in conselheiros})
+cores.update({node: 'green' for node in orgao})
 
-# grafo.add_edge("A", "B")
-# grafo.add_edge("A", "B")
-# grafo.add_edge("C", "C")
-# grafo.add_edge("A", "C")
+arestasCE = list(zip(conselheiros, siglas))
+arestasOE = list(zip(orgao, siglas))
 
-nx.draw(grafo, with_labels=True)
+grafo.add_edges_from(arestasCE, weight=1)
+grafo.add_edges_from(arestasOE, weight=1)
+
+teste = caminhoMinimo(grafo, 'Márcio Antônio Chiumento')
+
+def caminhoMinimo(grafo, origem):
+    # Inicialização das distâncias com infinito, exceto a origem que é zero
+    distancias = {v: sys.maxsize for v in grafo}
+    distancias[origem] = 0
+
+    # Conjunto de vértices visitados
+    visitados = set()
+
+    while visitados != set(distancias):
+        # Encontra o vértice não visitado com menor distância atual
+        vertice_atual = None
+        menor_distancia = sys.maxsize
+        for v in grafo:
+            if v not in visitados and distancias[v] < menor_distancia:
+                vertice_atual = v
+                menor_distancia = distancias[v]
+
+        # Marca o vértice atual como visitado
+        visitados.add(vertice_atual)
+
+        # Atualiza as distâncias dos vértices vizinhos
+        for vizinho, peso in grafo[vertice_atual].items():
+            if distancias[vertice_atual] + peso < distancias[vizinho]:
+                distancias[vizinho] = distancias[vertice_atual] + peso
+
+    # Retorna as distâncias mais curtas a partir da origem
+    return distancias
+    
+plt.figure(figsize=(10, 8))
+nx.draw_networkx(grafo, with_labels=True, node_color=[cores[node] for node in grafo.nodes()], node_size=500, font_size=12)
 plt.show()
